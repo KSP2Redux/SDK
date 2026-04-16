12fa2a5acb3c07b1905b8dde14b223dc3acfdfdf
Jan Bureš <jan.bures@live.com>
2026-04-16 06:13:18 +0200

    Fix file scoped namespace in SDKConfiguration

--- Editor/SDKConfiguration.cs
+++ Editor/SDKConfiguration.cs
@@ -1,10 +1,11 @@
-namespace Ksp2UnityTools.Editor;
-
-public static class SDKConfiguration
+namespace Ksp2UnityTools.Editor
 {
+    public static class SDKConfiguration
+    {
 #if REDUX
-    public const string BasePath = "Assets/Modules/KSP2UnityTools";
+        public const string BasePath = "Assets/Modules/KSP2UnityTools";
 #else
     public const string BasePath = "Packages/ksp2community.ksp2unitytools";
 #endif
+    }
 }
