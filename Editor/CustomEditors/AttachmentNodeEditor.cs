f154bdddedbdea0b5d45ca331c27d894abddce24
Jan Bureš <jan.bures@live.com>
2026-05-19 12:08:17 +0200

    Improved shroud editor and split up various custom editors into separate files

---
+++ Editor/CustomEditors/AttachmentNodeEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+
+namespace Ksp2UnityTools.Editor.CustomEditors
+{
+    [CustomEditor(typeof(AttachmentNode))]
+    public class AttachmentNodeEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            SerializedProperty sizeProperty =
+                serializedObject.FindProperty(PartSizeInspectorFields.AttachNodeSizeFieldName);
+            SerializedProperty sizeKeyProperty =
+                serializedObject.FindProperty(PartSizeInspectorFields.AttachNodeSizeKeyFieldName);
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+
+                if (iterator.propertyPath == "m_Script")
+                {
+                    using (new EditorGUI.DisabledScope(true))
+                    {
+                        EditorGUILayout.PropertyField(iterator, true);
+                    }
+
+                    continue;
+                }
+
+                if (iterator.name == PartSizeInspectorFields.AttachNodeSizeFieldName)
+                {
+                    PartSizeInspectorFields.DrawSizeKeyLayout(
+                        "Node Size",
+                        sizeKeyProperty,
+                        sizeProperty,
+                        PartSizeInspectorFields.LegacyPartSizeMode.AttachNode
+                    );
+                    continue;
+                }
+
+                if (iterator.name == PartSizeInspectorFields.AttachNodeSizeKeyFieldName)
+                {
+                    continue;
+                }
+
+                EditorGUILayout.PropertyField(iterator, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Editor/CustomEditors/PartDataDrawer.cs
@@ -0,0 +1,32 @@
+﻿using KSP.Sim.Definitions;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ksp2UnityTools.Editor.CustomEditors
+{
+    [CustomPropertyDrawer(typeof(PartData))]
+    public class PartDataDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return PartSizeInspectorFields.GetFoldoutPropertyHeight(
+                property,
+                PartSizeInspectorFields.PartSizeFieldName,
+                PartSizeInspectorFields.LegacyPartSizeCategoryFieldName
+            );
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            PartSizeInspectorFields.DrawFoldoutProperty(
+                position,
+                property,
+                label,
+                PartSizeInspectorFields.PartSizeFieldName,
+                PartSizeInspectorFields.LegacyPartSizeCategoryFieldName,
+                "Part Size",
+                PartSizeInspectorFields.LegacyPartSizeMode.PartCategory
+            );
+        }
+    }
+}
